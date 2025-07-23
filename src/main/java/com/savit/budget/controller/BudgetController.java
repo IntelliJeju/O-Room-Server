0bbb738433d5c04fe4bd92a9913c1cc18649cb14
kimjaehyeonn <jhjhh023@gmail.com>
2025-07-23 14:35:36 +0900

    feat: 카테고리별 예산 cru 기능 구현(#21)

--- src/main/java/com/savit/budget/controller/BudgetController.java
+++ src/main/java/com/savit/budget/controller/BudgetController.java
@@ -1,15 +1,21 @@
 package com.savit.budget.controller;
 
+import com.savit.budget.domain.BudgetCategoryVO;
 import com.savit.budget.domain.BudgetVO;
+import com.savit.budget.dto.BudgetCategoryDTO;
 import com.savit.budget.dto.BudgetDTO;
+import com.savit.budget.service.BudgetCategoryService;
 import com.savit.budget.service.BudgetService;
 import com.savit.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -18,13 +24,14 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 public class BudgetController {
     private final BudgetService budgetService;
+    private final BudgetCategoryService budgetCategoryService;
     private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<Void> createBudget(@RequestBody BudgetDTO dto, HttpServletRequest request) {
         Long userId = jwtUtil.getUserIdFromToken(request);
         budgetService.createBudget(dto, userId);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping
@@ -41,6 +48,33 @@ public class BudgetController {
     public ResponseEntity<BudgetVO> getBudget(HttpServletRequest request) {
         Long userId = jwtUtil.getUserIdFromToken(request);
         BudgetVO result = budgetService.getBudget(userId);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Void> createBudgetCategory(@RequestBody List<BudgetCategoryDTO> categories, HttpServletRequest request) {
+        Long userId = jwtUtil.getUserIdFromToken(request);
+        budgetCategoryService.createBudgetCategory(categories,userId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/categories")
+    public ResponseEntity<Void> changeBudgetCategory(@RequestBody List<BudgetCategoryDTO> categories, HttpServletRequest request) {
+        Long userId = jwtUtil.getUserIdFromToken(request);
+        int result = budgetCategoryService.changeBudgetCategory(categories,userId);
+        if ( result ==0 ){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/categories")
+    public ResponseEntity<Map<String, List<BudgetCategoryVO>>> getBudgetCategories(
+            @RequestParam(required = false) List<String> months,        // 여러 월
+            @RequestParam(required = false) List<Long> categoryIds,     // 여러 카테고리
+            HttpServletRequest request) {
+
+        Long userId = jwtUtil.getUserIdFromToken(request);
+        Map<String, List<BudgetCategoryVO>> result = budgetCategoryService.getBudgetCategories(userId, months, categoryIds);
         return ResponseEntity.ok(result);
     }
 }
