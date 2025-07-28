a95138f67c8f2a45f9e74887625048b854fdfdbd
hyun <nemo247300@gmail.com>
2025-07-28 15:14:36 +0900

    feat: 모든 카드 승인내역 + 사용자 보유카드 전체 id 값 가져오는 Mapper 추가

--- src/main/java/com/savit/card/mapper/CardApprovalMapper.java
+++ src/main/java/com/savit/card/mapper/CardApprovalMapper.java
@@ -30,4 +30,17 @@ public interface CardApprovalMapper {
      * @param cardId 조회할 카드 ID
      */
     List<CardApproval> findApprovalsByCardId(@Param("userId") Long userId, @Param("cardId") Long cardId);
+
+    /**
+     * 사용자의 특정 월 모든 카드 승인내역 조회
+     * @param userId 사용자 ID
+     * @param month 조회할 월 (yyyyMM 형식)
+     */
+    List<CardApproval> findThisMonthApprovalsByUser(@Param("userId") Long
+                                                            userId, @Param("month") String month);
+
+    /**
+     * 사용자 보유 카드 전체 id 값 가져오기
+     */
+    List<Long> findCardIdsByUser(Long userId);
 }
