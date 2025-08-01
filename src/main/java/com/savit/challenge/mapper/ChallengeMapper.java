de0391382918a7ce8df61432c880fb012490b013
soojeongchae <soojch12@gmail.com>
2025-08-01 16:28:30 +0900

    카드 승인 내역 ëªAPI 에 카테고리 분류 기능 추가

--- src/main/java/com/savit/challenge/mapper/ChallengeMapper.java
+++ src/main/java/com/savit/challenge/mapper/ChallengeMapper.java
@@ -1,11 +1,23 @@
 package com.savit.challenge.mapper;
 
 import com.savit.challenge.dto.ChallengeListDTO;
+import com.savit.notification.dto.ChallengeNotificationDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
 public interface ChallengeMapper {
     List<ChallengeListDTO> getChallengeList();
+
+    // 시작일 기준
+    List<ChallengeNotificationDTO> findByStartDate(@Param("startDate") LocalDate startDate);
+
+    // 종료일 기준
+    List<ChallengeNotificationDTO> findByEndDate(@Param("endDate") LocalDate endDate);
+
+    // 챌린지 ID로 참여 중인 유저 ID 리스트 조회
+    List<Long> findUserIdsByChallengeId(@Param("challengeId") Long challengeId);
 }
