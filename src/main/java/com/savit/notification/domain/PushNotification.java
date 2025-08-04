de6f7174736f4864d314a51b5cedd972f708e014
hyun <nemo247300@gmail.com>
2025-08-04 21:29:54 +0900

    refactor: PushNotification Builder 패턴 적용 및 푸시확인용 DB에 userId 저장 기능 추가

--- src/main/java/com/savit/notification/domain/PushNotification.java
+++ src/main/java/com/savit/notification/domain/PushNotification.java
@@ -1,9 +1,15 @@
 package com.savit.notification.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PushNotification {
     private Long id;
     private Long userId;
